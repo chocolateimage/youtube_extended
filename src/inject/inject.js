ba5d082193a215cfc87e1a26f87a31dd2131991b
chocolateimage <lukas@tunewar.de>
2022-09-21 20:02:57 +0200

    Fix Return YouTube Dislike counter invisible

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -3,6 +3,66 @@
 
 /* EXTENSIONS */
 
+function isMobile() {
+    return location.hostname == "m.youtube.com";
+}
+  
+function isShorts() {
+    return location.pathname.startsWith("/shorts");
+}
+function getButtons() {
+    //---   If Watching Youtube Shorts:   ---//
+    if (isShorts()) {
+      let elements = document.querySelectorAll(
+        isMobile()
+          ? "ytm-like-button-renderer"
+          : "#like-button > ytd-like-button-renderer"
+      );
+      for (let element of elements) {
+        //Youtube Shorts can have multiple like/dislike buttons when scrolling through videos
+        //However, only one of them should be visible (no matter how you zoom)
+        if (isInViewport(element)) {
+          return element;
+        }
+      }
+    }
+    //---   If Watching On Mobile:   ---//
+    if (isMobile()) {
+      return document.querySelector(".slim-video-action-bar-actions");
+    }
+    //---   If Menu Element Is Displayed:   ---//
+    if (document.getElementById("menu-container")?.offsetParent === null) {
+      return document.querySelector("ytd-menu-renderer.ytd-watch-metadata > div");
+      //---   If Menu Element Isnt Displayed:   ---//
+    } else {
+      return document
+        .getElementById("menu-container")
+        ?.querySelector("#top-level-buttons-computed");
+    }
+  }
+
+  function getDislikeButton() {
+    return getButtons().children[0].tagName ===
+      "YTD-SEGMENTED-LIKE-DISLIKE-BUTTON-RENDERER"
+      ? getButtons().children[0].children[1]
+      : getButtons().children[1];
+  }
+  
+  function getDislikeTextContainer() {
+    let result =
+      getDislikeButton().querySelector("#text") ??
+      getDislikeButton().getElementsByTagName("yt-formatted-string")[0] ??
+      getDislikeButton().querySelector("span[role='text']");
+    if (result == null) {
+      let textSpan = document.createElement("span");
+      textSpan.id = "text";
+      getDislikeButton().querySelector("button").appendChild(textSpan);
+      getDislikeButton().querySelector("button").style.width = "auto";
+      result = getDislikeButton().querySelector("#text");
+    }
+    return result;
+  }
+
 jQuery.postJSON = function(url,json,success) {
     var port = chrome.runtime.connect({name:"sendjson"});
     port.postMessage({"sendjson":{"url":url,"json":JSON.stringify(json)}});
@@ -844,6 +904,7 @@ function youtube_extended_loaded() {
     let oldid = null;
     let commentschanged = false;
     setInterval(function() {
+        getDislikeTextContainer()?.removeAttribute('is-empty');
         let newid = youtube_extended_videoid();
         let videochanged = oldid != newid;
         if (videochanged) {
